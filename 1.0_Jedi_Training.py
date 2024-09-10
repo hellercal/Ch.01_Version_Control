d38d33a6c9f2152ce29f06456695874464afe2fe
hellercal <hellercal@urbandaleschools.com>
2024-09-10 12:54:13 -0500

    everything

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -2,11 +2,11 @@
 1.0 Jedi Training (17pts)  Name:________________
 
 
-1. Define Forking (1pt): 
+1. Define Forking (1pt): Making a copy of a repository from an organization to my account on GitHub
 
-2. Define Cloning (1pt): 
+2. Define Cloning (1pt): Taking a repository from GitHub and putting it on my computer to work on
 
-3. Define Branching (1pt):
+3. Define Branching (1pt): 
 
 4. Define Committing (1pt): 
 
@@ -26,28 +26,74 @@ The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
-
-
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+tom = turtle.Turtle()
+screen = turtle.Screen()
+tom.speed(1000000)
+tom.pensize(4)
+switch = 0
+graphic_size = 252
+r4 = (11 / 12) * graphic_size
+r3 = (10 / 12) * graphic_size
+r2 = (9 / 12) * graphic_size
+r1 = (4 / 12) * graphic_size
+def circle(x):
+    tom.fillcolor()
+    tom.begin_fill()
+    tom.circle(x)
+    tom.end_fill()
+def bar(y):
+    tom.begin_fill()
+    for i in range(2):
+        tom.forward((4 / 3) * y)
+        tom.left(90)
+        tom.forward((1 / 9) * y)
+        tom.left(90)
+    tom.end_fill()
+def gap(z):
+    tom.begin_fill()
+    for i in range(4):
+        tom.forward((1 / 6) * z)
+        (tom .left(90))
+    tom.end_fill()
+tom.penup()
+for radius in [graphic_size, r4, r3, r2, r1]:
+    tom.forward(radius)
+    tom.left(90)
+    tom.pendown()
+    circle(radius)
+    tom.penup()
+    tom.home()
+    if switch == 0:
+        tom.color("white")
+        switch = 1
+        continue
+    tom.color("black")
+    switch = 0
+tom.color("black")
+for angle in [0, 45, 90, 135]:
+    tom.left(angle)
+    tom.backward((2 / 3) * graphic_size)
+    tom.right(90)
+    tom.forward((1 / 18) * graphic_size)
+    tom.left(90)
+    tom.pendown()
+    bar(graphic_size)
+    tom.penup()
+    tom.home()
+tom.color("white")
+for i in range(8):
+    tom.right(90)
+    tom.forward((1 / 12) * graphic_size)
+    tom.left(90)
+    tom.forward((178.5 / 252) * graphic_size)
+    tom.pendown()
+    gap(graphic_size)
+    tom.penup()
+    tom.home()
+    tom.left((1 + i) * 45)
+tom.penup()
+tom.goto(200,-200)
+tom.pendown()
+tom.pencolor("black")
+tom.write('Calen Heller',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
